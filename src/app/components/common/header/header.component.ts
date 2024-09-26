fefae7a015c153a5dd32d9a91fb92cbb916e8297
nicobolton2 <nicoboltongithub@gmail.com>
2024-09-26 13:02:35 -0300

    nuevo commit

--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -1,51 +1,50 @@
-import { Component, ViewChild } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-// import { NarvarComponent } from '../narvar/narvar.component';
-import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
-import { MatToolbar } from '@angular/material/toolbar';
-import { MatNavList } from '@angular/material/list';
-import { MatIcon } from '@angular/material/icon';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { Component, ChangeDetectorRef, OnDestroy, inject } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    RouterLink,
-    MatSidenav,
-    MatSidenavContainer,
-    MatToolbar,
-    MatSidenavContent,
-    MatNavList,
-    MatIcon,
-    CommonModule
+    MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule
 
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  isHandset$!: Observable<boolean>;
+  mobileQuery: MediaQueryList;
 
-  @ViewChild('sidenav') sidenav!: MatSidenav;
-  
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  fillerNav = Array.from({length: 3}, (_, i) => `Nav Item ${i + 1}`);
 
-  ngOnInit(): void {
-    // Aquí inicializamos isHandset$ después de que el breakpointObserver ha sido inyectado.
-    this.isHandset$ = this.breakpointObserver.observe(Breakpoints.Handset)
-      .pipe(
-        map(result => result.matches),
-        shareReplay()
-      );
+  fillerContent = Array.from(
+    {length: 3},
+    () =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+  );
+
+  private _mobileQueryListener: () => void;
+
+  constructor() {
+    const changeDetectorRef = inject(ChangeDetectorRef);
+    const media = inject(MediaMatcher);
+
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
 
-  toggleSidenav() {
-    this.sidenav.toggle();
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
-  
+
 }
